f1db2795ba77bef568d9dda977cfba740fcc2140
udhayamagi <udhayarshmi@gmail.com>
2025-03-17 13:40:52 +0530

    New Project Push

---
+++ src/test/java/org/test/Term1.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Term1 {
+	
+	private void class1() {
+		System.out.println("class1");
+
+	}
+	private void class2() {
+		System.out.println("class2");
+
+	}
+	private void class3() {
+		System.out.println("class3");
+
+	}
+	
+
+}
